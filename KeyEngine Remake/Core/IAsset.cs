fd095d084f4b183d0e0c096f5407220bbb61b04b
KeyDev <keydevtutor@gmail.com>
2025-06-26 01:58:35 +0300

    Lmao

---
+++ KeyEngine Remake/Core/IAsset.cs
@@ -0,0 +1,10 @@
+﻿namespace KeyEngine.Editor
+{
+    public interface IAsset
+    {
+        public void LoadAsset(string path);
+        public void UnloadAsset();
+
+        public bool AssetLoaded { get; }
+    }
+}
