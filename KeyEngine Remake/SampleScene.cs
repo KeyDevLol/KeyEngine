e6ec9b9fa8de754be191e59ee7e5d2276e7bd205
KeyDev <keydevtutor@gmail.com>
2025-06-25 22:56:09 +0300

    Create SampleScene.cs

---
+++ KeyEngine Remake/SampleScene.cs
@@ -0,0 +1,29 @@
+﻿namespace KeyEngine
+{
+    public class SampleScene : IScene
+    {
+        public void Load()
+        {
+            // Creating a new entity
+            Entity entity = ECS.AddEntity("Entity Name");
+
+            // Adding new component to entity
+            entity.AddComponent<SpriteRenderer>();
+
+            // Getting a component and changing its variable
+            SpriteRenderer? spriteRenderer = entity.GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null)
+                spriteRenderer.Color = Color.Red;
+            else
+                Log.Print("SpriteRenderer is null!", LogType.Error);
+
+            Log.Print("SampleScene Loaded!");
+        }
+
+        public void Unload()
+        {
+            // Called when the scene is unloaded.
+            Log.Print("SampleScene Unloaded.");
+        }
+    }
+}
