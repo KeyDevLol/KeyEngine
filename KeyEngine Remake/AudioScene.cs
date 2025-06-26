50f2bb71d3d93d24658cfb204177bf33161565a1
KeyDev <keydevtutor@gmail.com>
2025-06-26 03:27:08 +0300

    Create AudioScene.cs

---
+++ KeyEngine Remake/AudioScene.cs
@@ -0,0 +1,17 @@
+﻿using KeyEngine.Audio;
+
+namespace KeyEngine
+{
+    public class AudioScene : IScene
+    {
+        public void Load()
+        {
+            Entity audioSourceEntity = ECS.AddEntity("AudioSource");
+            AudioSource audioSource = audioSourceEntity.AddComponent<AudioSource>();
+            audioSource.AudioSample = new AudioSample("Assets/Audio/Vigilantism.wav");
+            audioSource.Play();
+        }
+
+        public void Unload() { }
+    }
+}
