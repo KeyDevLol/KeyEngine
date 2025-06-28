d7156dcbc1e00d706922c093afd93c090eb1b6d1
KeyDev <keydevtutor@gmail.com>
2025-06-28 03:13:14 +0300

    added more features

--- KeyEngine Remake/Math/Mathf.cs
+++ KeyEngine Remake/Math/Mathf.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KeyEngine
+﻿namespace KeyEngine
 {
     public struct Mathf
     {
@@ -73,8 +67,45 @@ namespace KeyEngine
                 return value;
         }
 
-        public const float Deg2Rad = PI * 2f / 360f;
-        public const float Rad2Deg = 1f / Deg2Rad;
+        public static float Repeat(float t, float length)
+        {
+            return Clamp(t - Floor(t / length) * length, 0.0f, length);
+        }
+
+        public static float Pow(float f, float p) { return (float)Math.Pow(f, p); }
+
+        public static float Exp(float power) { return (float)Math.Exp(power); }
+
+        public static float Log(float f, float p) { return (float)Math.Log(f, p); }
+
+        public static float Log(float f) { return (float)Math.Log(f); }
+
+        public static float Log10(float f) { return (float)Math.Log10(f); }
+
+        public static float Ceil(float f) { return (float)Math.Ceiling(f); }
+
+        public static float Floor(float f) { return (float)Math.Floor(f); }
+
+        public static float Round(float f) { return (float)Math.Round(f); }
+
+        public static int CeilToInt(float f) { return (int)Math.Ceiling(f); }
+
+        public static int FloorToInt(float f) { return (int)Math.Floor(f); }
+
+        public static int RoundToInt(float f) { return (int)Math.Round(f); }
+
+        public static float Sign(float f) { return f >= 0F ? 1F : -1F; }
+
         public const float PI = (float)Math.PI;
+
+        public const float INFINITY = float.PositiveInfinity;
+
+        public const float NEGITIVE_INFINITY = float.NegativeInfinity;
+
+        public const float DEG_2_RAD = PI * 2F / 360F;
+
+        public const float RED_2_DEG = 1F / DEG_2_RAD;
+
+        internal const int kMaxDecimals = 15;
     }
 }
