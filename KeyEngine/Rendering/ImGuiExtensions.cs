de4c3b2744ba7149b5a5794a963348e98a1f06ee
KeyDev <keydev28@gmail.com>
2025-11-23 05:11:08 +0300

    Added ImGuiExtensions
    .NET 10 extension wowie

---
+++ KeyEngine/Rendering/ImGuiExtensions.cs
@@ -0,0 +1,35 @@
+﻿using ImGuiNET;
+using System.Numerics;
+
+namespace KeyEngine.Rendering
+{
+    public static class ImGuiExtensions
+    {
+        extension(ImGui)
+        {
+            public static bool InspectorVariable(string nameText, string valueName, float valueSizePercentage = 0.5f, float buttonHeight = 10)
+            {
+                InspectorVariableText(nameText);
+
+                return InspectorVariableValue(nameText, valueSizePercentage, buttonHeight);
+            }
+
+            public static void InspectorVariableText(string nameText)
+            {
+                ImGui.Text(nameText);
+                ImGui.SameLine();
+            }
+
+            public static bool InspectorVariableValue(string valueName, float valueSizePercentage = 0.5f, float buttonHeight = 10)
+            {
+                float remaining = ImGui.GetContentRegionAvail().X;
+                float buttonWidth = remaining * valueSizePercentage;
+                buttonHeight += ImGui.GetItemRectSize().Y;
+
+                ImGui.SetCursorPosX(ImGui.GetCursorPosX() + remaining - buttonWidth);
+
+                return ImGui.Button(valueName, new Vector2(buttonWidth, buttonHeight));
+            }
+        }
+    }
+}
