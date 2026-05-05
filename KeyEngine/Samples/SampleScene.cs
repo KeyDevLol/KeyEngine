ffbdcf1d8f512b453afb579f0321095933f12a05
KeyDev <keydev28@gmail.com>
2026-05-05 03:12:33 +0300

    Little changes

--- KeyEngine/Samples/SampleScene.cs
+++ KeyEngine/Samples/SampleScene.cs
@@ -1,4 +1,5 @@
-﻿using KeyEngine.Mathematics;
+﻿using KeyEngine.Physics;
+using KeyEngine.Mathematics;
 
 namespace KeyEngine.Samples
 {
@@ -7,24 +8,25 @@ namespace KeyEngine.Samples
         public void Load()
         {
             // Creating a new entity
-            Entity entity = ECS.AddEntity("Entity Name");
+            Entity entity = ECS.AddEntity("Cute White Square");
 
             // Adding new component to entity
             entity.AddComponent<SpriteRenderer>();
+            entity.AddComponent<RigidBody>();
 
-            // Getting a component and changing its variable
+            // Getting a component
             SpriteRenderer? spriteRenderer = entity.GetComponent<SpriteRenderer>();
             if (spriteRenderer != null)
-                spriteRenderer.Color = Color32.Red;
+                spriteRenderer.Color = Color32.White;
             else
-                Log.Print("SpriteRenderer is null!", LogType.Error);
+                Log.Print("SpriteRenderer is null.", LogType.Error);
 
-            Log.Print("SampleScene Loaded!");
+            Log.Print("SampleScene Loaded.");
         }
 
         public void Unload()
         {
-            // Called when the scene is unloaded.
+            // Called when the scene is unloaded
             Log.Print("SampleScene Unloaded.");
         }
     }
