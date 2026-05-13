2a26f5cab3ab8601217eaca60b763c41fe19d1ea
KeyDev <keydev28@gmail.com>
2026-05-14 01:50:07 +0300

    Fix

--- Build/build_linux.bat
+++ Build/build_linux.bat
@@ -1,6 +1,6 @@
 @echo off
 dotnet publish "../KeyEngine" --output "Linux" --sc true -r linux-x64 -p:PublishSingleFile=true -c Release
-robocopy "../KeyEngine/Content/Assets" "Windows/Assets" /E /COPY:DAT /R:3 /W:10
-robocopy "../KeyEngine/Content/Editor" "Windows/Editor" /E /COPY:DAT /R:3 /W:10
+robocopy "../KeyEngine/Content/Assets" "Linux/Assets" /E /COPY:DAT /R:3 /W:10
+robocopy "../KeyEngine/Content/Editor" "Linux/Editor" /E /COPY:DAT /R:3 /W:10
 echo Build completed.
 pause
