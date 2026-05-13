2914dfcc5afda3683d7892478c903e8808961f78
KeyDev <keydev28@gmail.com>
2026-05-14 01:41:44 +0300

    Renamed ProjectTemplate

---
+++ KeyEngine Workshop/Projects/ProjectTemplateInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace KeyEngine_Workshop.Projects
+{
+    public struct ProjectTemplateInfo
+    {
+        public string DisplayName { get; set; }
+        public string Description { get; set; }
+        [JsonIgnore]
+        public string Path { get; set; }
+
+        public ProjectTemplateInfo(string displayName, string description, string path)
+        {
+            DisplayName = displayName;
+            Description = description;
+            Path = path;
+        }
+    }
+}
