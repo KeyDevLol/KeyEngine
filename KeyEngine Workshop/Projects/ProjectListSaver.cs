bd1409ed86400f904e1ee4decefb1f8bc1088135
KeyDev <keydev28@gmail.com>
2026-05-14 01:38:30 +0300

    Create ProjectListSaver.cs

---
+++ KeyEngine Workshop/Projects/ProjectListSaver.cs
@@ -0,0 +1,13 @@
+﻿using KeyEngine_Workshop.Windowing;
+using System.ComponentModel;
+
+namespace KeyEngine_Workshop.Projects
+{
+    public class ProjectListSaver : OpenTKWindowHandlerBase
+    {
+        public override void OnWindowClosing(CancelEventArgs e)
+        {
+            ProjectManager.SaveProjectList();
+        }
+    }
+}
