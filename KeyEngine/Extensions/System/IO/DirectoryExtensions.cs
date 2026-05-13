8a9df330e3f45a82f8bf4b23acc970a34e1b9a6b
KeyDev <keydev28@gmail.com>
2026-05-14 01:44:01 +0300

    Create DirectoryExtensions.cs

---
+++ KeyEngine/Extensions/System/IO/DirectoryExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace KeyEngine.Extensions.System.IO
+{
+    public static class DirectoryExtensions
+    {
+        extension (Directory)
+        {
+            public static void Copy(string sourcePath, string targetPath)
+            {
+                foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+                {
+                    Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+                }
+
+                foreach (string newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+                {
+                    File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
+                }
+            }
+        }
+    }
+}
