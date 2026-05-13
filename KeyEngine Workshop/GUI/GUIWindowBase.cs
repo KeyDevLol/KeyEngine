6d2f3f079e389f8cea5467cd2ad1b7475cfe6795
KeyDev <keydev28@gmail.com>
2026-05-14 01:39:04 +0300

    Update GUIWindowBase.cs

--- KeyEngine Workshop/GUI/GUIWindowBase.cs
+++ KeyEngine Workshop/GUI/GUIWindowBase.cs
@@ -5,7 +5,7 @@ namespace KeyEngine_Workshop.GUI
     public abstract class GuiWindowBase
     {
         public bool IsVisible { get => _isVisible; set => _isVisible = value; }
-        private bool _isVisible = true;
+        protected bool _isVisible = true;
         public virtual string Title { get; set; } = "ImGuiWindow";
         protected virtual ImGuiWindowFlags WindowFlags { get; set; }
 
@@ -15,12 +15,25 @@ namespace KeyEngine_Workshop.GUI
                 return;
 
             PreBegin();
-            if (ImGui.Begin($"{Title}", ref _isVisible, WindowFlags))
+            
+            if (Begin())
             {
                 DrawContent();
             }
+
+            End();
+        }
+
+        protected virtual bool Begin() 
+        {
+            return ImGui.Begin(Title, ref _isVisible, WindowFlags);
+        }
+
+        protected virtual void End()
+        {
             ImGui.End();
         }
+
         public virtual void OnUpdateFrame(float deltaTime) { }
         public virtual void OnRegister() { }
         protected virtual void PreBegin() { }
