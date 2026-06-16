7e550bd47e29287cbe4b3434b3164705291ca61b
sonesoul <smippy098@gmail.com>
2026-06-15 20:26:06 -0600

    add SetSeed method

--- KeyEngine/Mathematics/Rand.cs
+++ KeyEngine/Mathematics/Rand.cs
@@ -1,15 +1,16 @@
-﻿using NAudio.Codecs;
-using System.Numerics;
+﻿using System.Numerics;
 
 namespace KeyEngine.Mathematics
 {
     public static class Rand
     {
-        private static Random rand = new Random();
-        
-        public static int Range(int min, int max) => rand.Next(min, max);
-        public static double Range(double min, double max) => Lerp(min, max, rand.NextSingle());
-        public static float Range(float min, float max) => Lerp(min, max, rand.NextSingle());
+        private static Random Rng { get; set; } = new Random();
+
+        public static void SetSeed(int seed) => Rng = new(seed);
+
+        public static int Range(int min, int max) => Rng.Next(min, max);
+        public static double Range(double min, double max) => Lerp(min, max, Rng.NextSingle());
+        public static float Range(float min, float max) => Lerp(min, max, Rng.NextSingle());
 
         private static T Lerp<T>(T min, T max, T range01) where T : INumber<T>
         {
